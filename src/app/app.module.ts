b5bb9e099754e18021138bae322750fedd33770c
Pedro Rivaldo <pedrorivaldev@gmail.com>
2021-03-17 16:37:16 -0300

    code copy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './core/components/sidebar/sidebar.component';
 import { LogoComponent } from './core/components/logo/logo.component';
 import { GradientComponent } from './core/components/gradient/gradient.component';
+import { ClipboardModule } from 'ngx-clipboard';
 
 @NgModule({
   declarations: [
@@ -15,8 +16,10 @@ import { GradientComponent } from './core/components/gradient/gradient.component
     GradientComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClipboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/components/gradient/gradient.component.ts
+++ src/app/core/components/gradient/gradient.component.ts
@@ -1,27 +1,51 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { ClipboardService } from 'ngx-clipboard';
 
 @Component({
   selector: 'app-gradient',
   templateUrl: './gradient.component.html',
-  styleUrls: ['./gradient.component.css']
+  styleUrls: ['./gradient.component.css'],
 })
 export class GradientComponent implements OnInit {
+  code: string = '';
+  codeHTML: string = '';
 
-  colors = [];
+  public ctrlColors = [
+    { control: 'First', color: '#72e3ea' },
+    { control: 'Second', color: '#bd98bd' },
+  ];
 
-  constructor() { }
+  constructor(public clipboardService: ClipboardService) {}
 
   ngOnInit(): void {
-    this.getColors();
-    document.getElementById('result').style.background = `linear-gradient(to bottom, ${this.colors[0]} 0%, ${this.colors[1]} 100%)`;
+    this.setColors();
   }
 
-  getColors() {
-    this.colors = [];
-    let inputColors = document.querySelectorAll(".gradient-color-input");
-    inputColors.forEach((inputColor: any) => {
-      this.colors.push(inputColor.value);
-    });
+  copy(text: string) {
+    this.clipboardService.copy(text);
   }
 
+  setColors() {
+    document.getElementById(
+      'result'
+    ).style.background = `linear-gradient(to bottom, ${this.ctrlColors[0].color} 0%, ${this.ctrlColors[1].color} 100%)`;
+  }
+
+  setCode() {
+    this.code = `background:
+    linear-gradient
+      (to bottom,
+      ${this.ctrlColors[0].color} 0%,
+      ${this.ctrlColors[1].color} 100%);
+  `;
+    this.codeHTML = `
+      <span class="code-bg">background:</span>
+      <span class="code-gradient">linear-gradient</span>
+        (to bottom,
+        <span class="code-color" style="color: ${this.ctrlColors[0].color}">${this.ctrlColors[0].color}</span> 0%,
+        <span class="code-color" style="color: ${this.ctrlColors[1].color}">${this.ctrlColors[1].color}</span> 100%);
+    `;
+    document.getElementById('code').innerHTML = this.codeHTML;
+  }
 }
